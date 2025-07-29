3f1cb4d40e7b77397729d7e57d71103c77d5641d
noyoungchul <shdudcjf107@gmail.com>
2025-07-29 10:31:51 +0900

    BGA 변환
    BGA 변환

--- src/color.py
+++ src/color.py
@@ -3,8 +3,16 @@ import numpy as np
 
 # 기본값
 img = cv2.imread('../img/like_lenna.png')
+# bgr
+bgr = cv2.imread('../img/like_lenna.png', cv2.IMREAD_COLOR)
+# alpha
+bgra = cv2.imread('../img/like_lenna.png', cv2.IMREAD_UNCHANGED)
+#shape
+print("default", img.shape, "color", bgr.shape, "unchanged", bgra.shape)
 
 cv2.imshow('img', img)
+cv2.imshow('bgr', bgr)
+cv2.imshow('alpha', bgra[:,:,3])
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
